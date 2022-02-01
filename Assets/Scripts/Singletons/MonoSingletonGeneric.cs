07f70397344e10b5c576aa7b6b8ef75dbf029783
punitp59 <punitp59@gmail.com>
2022-02-01 18:50:51 +0530

    Update 1 - Singleton
    Singleton structure initialized into the project for enemy Tank and player Tank player within a generic singleton structure code called MonoSingletonGeneric

---
+++ Assets/Scripts/Singletons/MonoSingletonGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MonoSingletonGeneric<T> : MonoBehaviour where T: MonoSingletonGeneric<T> {
+	public static T Instance {get {return instance;}}
+	private void Awake() {
+		if (Instance == null){
+			Instance = (T)this;
+		}
+		else{
+			Destroy(this);
+		}
+	}
+}
+
+public class PlayerTank : MonoSingletonGeneric<PlayerTank> {
+
+}
+public class EnemyTank : MonoSingletonGeneric<EnemyTank> {
+	
+}
